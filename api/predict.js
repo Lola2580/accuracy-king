f2aafbc4083a5f02f89d9008ec33013a7e916e6a
Lola2580 <royranjita963@gmail.com>
2026-02-17 19:42:44 +0530

    predict.js

--- api/predict.js
+++ api/predict.js
@@ -3,8 +3,14 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.SUPABASE_SERVICE_KEY,
+  {
+    auth: {
+      persistSession: false
+    }
+  }
 );
+
 
 export default async function handler(req, res) {
   try {
